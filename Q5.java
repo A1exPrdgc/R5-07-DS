96778eff1eecd9b394cf8bcb197177c6e0cff40f
A1exandre Pradignac <pa230262@etu.univ-lehavre.fr>
2025-10-08 16:43:52 +0200

    fix

---
+++ Q5.java
@@ -0,0 +1,25 @@
+public class Q5
+{
+    public static void main(String[] args) {
+        if(args[0] != null)
+        {
+            int fin = Integer.parseInt(args[0]);
+
+            String finalStr = "";
+            int cpt = fin;
+            while (cpt >= 0) 
+            {
+                String s = "";
+                for (int i = 0; i < cpt; i++) 
+                {
+                    s += i + " ";
+                }
+                s += "\n";
+                finalStr += s;
+                cpt--;
+            }
+            System.out.println(finalStr);  
+        } 
+    }
+}
+
